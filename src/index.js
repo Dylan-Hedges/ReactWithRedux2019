305155093570518e134cf3162b3274b097deb1e9
DylanHedges <d.hedges1@hotmail.co.uk>
2019-01-04 18:08:37 -0500

    Created CommentDetail component, moved JSX, passed in props

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return(
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date"> Today at 6:00pm</span>
-          </div>
-          <div className="text"> Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} timeAgo="Yesterday at 6:00pm" content={faker.lorem.sentence()}/>
+      <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} timeAgo="Yesterday at 7:00pm" content={faker.lorem.sentence()}/>
+      <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} timeAgo="Yesterday at 8:00pm" content={faker.lorem.sentence()}/>
     </div>
   );
 };
