74ff88d9a57d5c22e2767a458d59d2ed8cf35eb0
DylanHedges <d.hedges1@hotmail.co.uk>
2019-01-05 08:43:37 -0500

    Added ApprovalCard component, passed in CommentDetail component as props - passed one component into another as props

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return(
     <div className="ui container comments">
-      <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} timeAgo="Yesterday at 6:00pm" content={faker.lorem.sentence()}/>
-      <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} timeAgo="Yesterday at 7:00pm" content={faker.lorem.sentence()}/>
-      <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} timeAgo="Yesterday at 8:00pm" content={faker.lorem.sentence()}/>
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} timeAgo="Yesterday at 6:00pm" content={faker.lorem.sentence()}/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} timeAgo="Yesterday at 7:00pm" content={faker.lorem.sentence()}/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail avatar={faker.image.avatar()} author={faker.name.firstName()} timeAgo="Yesterday at 8:00pm" content={faker.lorem.sentence()}/>
+      </ApprovalCard>
     </div>
   );
 };
