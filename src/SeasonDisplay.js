eb43fdb4cd10fe2508e8ee84e6a18f339290a1e5
DylanHedges <d.hedges1@hotmail.co.uk>
2019-03-21 07:35:12 -0400

    Added seasonConfig and on-screen elements

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,17 @@
 import React from 'react';
-//Function determines if its winter or summer
+
+//Stores on screen text and icon classes
+const seasonConfig ={
+  summer: {
+    text: "Lets hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr its cold",
+    iconName: "snowflake"
+  }
+}
+//Function that determines if its winter or summer
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9){
     return lat > 0 ? 'summer' : 'winter';
@@ -8,9 +20,17 @@ const getSeason = (lat, month) => {
   }
 }
 const SeasonDisplay = (props) => {
-  //Passes lat and month (number) into function
+  //Executes function and stores if it is winter or summer- Passes lat and month (number) into getSeason function
   const season = getSeason(props.lat, new Date().getMonth());
-  return <div>SeasonDisplay</div>
+  //Stores season text and icon class - e.g winter.text & winter.iconName
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div>
+      <i className={`massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`massive ${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
