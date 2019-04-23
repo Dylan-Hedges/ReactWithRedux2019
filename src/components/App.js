34a67734eaddb4e8e3c51d27af5e137b7aa94804
DylanHedges <d.hedges1@hotmail.co.uk>
2019-04-23 07:50:56 -0400

    Added search query for Axois

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 import React from 'react';
 import SearchBar from './SearchBar';
+import {unsplashedApiKey} from '../config.js';
 
 class App extends React.Component{
   onSearchSubmit(term){
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos',{
+      params: { query: term},
+      headers:{
+        Authorization: `Client-ID ${unsplashedApiKey}`
+      }
+    });
   }
 
   render(){
