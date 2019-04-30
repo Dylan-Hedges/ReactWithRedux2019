fea7fd599a93a4e555fcc5e8d361cf5650756b64
DylanHedges <d.hedges1@hotmail.co.uk>
2019-04-30 08:13:47 -0400

    Added redux and react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
-class App extends React.Component{
-  render(){
-    return(
-      <div>
-        APP
-      </div>
-    );
-  }
-}
+const App = () => {
+ return <div>App</div>;
+};
 
 export default App;
