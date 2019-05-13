c4ae548732d5ded413a609514bed3ae4f0a680d8
DylanHedges <d.hedges1@hotmail.co.uk>
2019-05-13 08:06:58 -0400

    Inital commit - blog app - created boilerplate setup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,5 @@
 import { combineReducers } from 'redux';
 
-//-----REDUCERS-----
-const songsReducer = () => {
-  return[
-    {title: 'No Scrubs', duration: '4:05'},
-    {title: 'Macarena', duration: '2:30'},
-    {title: 'All Star', duration: '3:10'},
-    {title: 'I Want It That Way', duration: '1:45'},
-  ];
-};
-
-const selectedSongReducer = (selectedSong = null, action) => {
-  if (action.type === 'SONG_SELECTED'){
-    return action.payload;
-  }
-  return selectedSong;
-};
-
 export default combineReducers({
-    songs: songsReducer,
-    selectedSong: selectedSongReducer
+  replaceMe: () => 'hi there'
 });
