7ab02d5b24b9a0418fef2f7345fc1d80add83c91
DylanHedges <d.hedges1@hotmail.co.uk>
2019-06-04 19:30:08 -0400

    Added Memoized (Lodash) solution for overfetching problem (fetching user names)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
+import _ from 'lodash';
 //Stores the first part of the API URL
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-//Action Creator that returns a function - Redux Thunk needs to be wired up to do this
+//Fetch Posts Action Creator - returns a function - Redux Thunk needs to be wired up to do this
 export const fetchPosts = () => {
   //Returns a function
   return async (dispatch) => {
@@ -12,7 +13,12 @@ export const fetchPosts = () => {
   };
 };
 
-export const fetchUser = (id) => async dispatch => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
-  dispatch({ type: 'FETCH_USER', payload: response.data });
+//Fetch User Names Action Creator - Calls the memoized function
+export const fetchUser = (id) => dispatch => {
+  _fetchUser(id, dispatch);
 };
+//Fetch User Names Memoized Function - Part of Lodash calls the Action Creator ONLY ONE TIME with each UNIQUE user id
+const _fetchUser = _.memoize(async(id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+});
