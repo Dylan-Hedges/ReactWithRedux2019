068714277c2ed193e03b952a2ea4af439370e9e2
DylanHedges <d.hedges1@hotmail.co.uk>
2019-06-05 07:49:16 -0400

    Replaced Memoized solution for overfetching API requests with alternative solution - created new larger AC that fetches both posts and user info

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,20 @@ import _ from 'lodash';
 //Stores the first part of the API URL
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-//Fetch Posts Action Creator - returns a function - Redux Thunk needs to be wired up to do this
+//Fetches Posts & User Info - Prevents overfetching (multiple requests from components)
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  //Calls Fetch Posts action creator
+  await dispatch(fetchPosts());
+
+  //Gets list of posts.Maps over userId.Finds unique ids.runs fetchUser() AC with each id.executes the chain
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch (fetchUser(id)))
+    .value()
+};
+
+//Fetch Posts action areator - returns a function - Redux Thunk needs to be wired up to do this
 export const fetchPosts = () => {
   //Returns a function
   return async (dispatch) => {
@@ -13,12 +26,25 @@ export const fetchPosts = () => {
   };
 };
 
-//Fetch User Names Action Creator - Calls the memoized function
-export const fetchUser = (id) => dispatch => {
-  _fetchUser(id, dispatch);
+//Fetch User Info action creator - Fetches user info
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({type: 'FETCH_USER', payload: response.data});
 };
-//Fetch User Names Memoized Function - Part of Lodash calls the Action Creator ONLY ONE TIME with each UNIQUE user id
-const _fetchUser = _.memoize(async(id, dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+
+//-----Alternative to Chain--------
+//Gets the unique id for each user and stores in new array
+//const userIds = _.uniq(_.map(getState().posts, 'userId'));
+//Calls the Fetch User Info action creator - calls with unique id to prevent overfetching
+//userIds.forEach(id => dispatch (fetchUser(id)));
+
+//-----------Memoized Solution------------------
+//Fetch User Names Action Creator - Calls the memoized function
+// export const fetchUser = (id) => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+// //Fetch User Names Memoized Function - Part of Lodash calls the Action Creator ONLY ONE TIME with each UNIQUE user id
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
